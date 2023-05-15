386b16613ac925671fc006076f9ba3fbea64c12e
Lou Andria <lou@hei.school>
2023-05-15 17:39:34 +0200

    feat: dummy dreactivateFiles and getFileById handlers

---
+++ Function/src/main/java/taratasy/handler/DreactivateFileHandler.java
@@ -0,0 +1,30 @@
+package taratasy.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import taratasy.security.authorization.Operation;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import static taratasy.security.authorization.Operation.DREACTIVATE;
+
+public class DreactivateFileHandler extends SecuredRequestHandler {
+
+  public DreactivateFileHandler() throws URISyntaxException, MalformedURLException {
+    super();
+  }
+
+  @Override
+  protected APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+    return new APIGatewayProxyResponseEvent()
+        .withStatusCode(200)
+        .withBody("{ \"message\": \"ok\" }");
+  }
+
+  @Override
+  protected Operation getTargetOperation() {
+    return DREACTIVATE;
+  }
+}
---
+++ Function/src/main/java/taratasy/handler/GetFileByIdHandler.java
@@ -0,0 +1,30 @@
+package taratasy.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import taratasy.security.authorization.Operation;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import static taratasy.security.authorization.Operation.READ;
+
+public class GetFileByIdHandler extends SecuredRequestHandler {
+
+  public GetFileByIdHandler() throws URISyntaxException, MalformedURLException {
+    super();
+  }
+
+  @Override
+  protected APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+    return new APIGatewayProxyResponseEvent()
+        .withStatusCode(200)
+        .withBody("{ \"message\": \"ok\" }");
+  }
+
+  @Override
+  protected Operation getTargetOperation() {
+    return READ;
+  }
+}
